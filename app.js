69070a964891aa1b5f6f25634ae394034e7f84a1
Your Name <soniyagupta82270@gmail.com>
2024-10-08 00:04:13 +0530

    add rate limiting and protection from XSS attacks

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import userRouter from "./src/routes/user.routes.js";
-
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 const app = express();
+app.use(helmet());
+
+// Define a global rate limit
+const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again later",
+});
+
+// Apply the rate-limiter middleware to all requests
+app.use(globalLimiter);
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
